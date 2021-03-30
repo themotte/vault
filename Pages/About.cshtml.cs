0d7fa6a302b1c20353c3c54fa53cf95fedca2c7c
Akamitori <akamitori@tutanota.com>
2021-03-30 18:28:57 +0300

    Added about pagedand made sure the routing worked for all the navigation between the various pages(small oversight_

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QCVault.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly ILogger<AboutModel> _logger;
+
+        public AboutModel(ILogger<AboutModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
