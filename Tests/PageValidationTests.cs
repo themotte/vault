ec8c79779e2af80ef4413d6b7778c9dd65293090
Akamitori <78937210+Akamitori@users.noreply.github.com>
2021-05-27 18:12:59 +0300

    Automate page validation test.

---
+++ Tests/PageValidationTests.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using NUnit.Framework;
+using QCUtilities;
+
+namespace QCVault.Tests
+{
+    [TestFixture]
+    public class PageValidationTests
+    {
+        private WebApplicationFactory<QCVault.Startup> factory;
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            factory =
+                new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder => builder.UseSolutionRelativeContentRoot(""));
+        }
+
+        [OneTimeTearDown]
+        public void Teardown()
+        {
+            factory?.Dispose();
+        }
+
+
+        public PostDeserializer CreateDeserializer()
+        {
+            string xmlPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Posts", "posts");
+            string xsd = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Posts", "posts.xsd");
+            return new PostDeserializer(new DiskArchiveValidator(), new CollectionValidator(), xmlPath, xsd);
+        }
+        [TestCase("/")]
+        [TestCase("/About")]
+        [TestCase("/Error")]
+        [TestCase("/PostList")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.AreEqual("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+        [Test]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType_ForPosts()
+        {
+            var client = factory.CreateClient();
+
+            string xmlPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Posts", "posts");
+            
+            var des = CreateDeserializer();
+
+            var posts = des.Posts;
+            foreach (var post in posts)
+            {
+                string url = $@"/post/{post.URLSlug}";
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode(); // Status Code 200-299
+                Assert.AreEqual("text/html; charset=utf-8",
+                    response.Content.Headers.ContentType.ToString());
+            }
+        }
+
+        [TestCase("/GibberishForever")]
+        [TestCase("/post/GibberishForever")]
+        public async Task Get_EndpointsReturnFailure_ForInvalidPost(string url)
+        {
+            var client = factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+
+            
+            Assert.AreEqual(null,
+                response.Content.Headers.ContentType);
+            Assert.AreEqual(response.IsSuccessStatusCode, false);
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.NotFound);
+        }
+    }
+}
+
