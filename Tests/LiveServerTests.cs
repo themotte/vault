e770a4083ba1063117af5d4f6deafb804602c42e
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2021-07-17 00:17:00 -0500

    Move live-server-relevant test harness into a parent class.

---
+++ Tests/LiveServerTests.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using NUnit.Framework;
+using QCUtilities;
+
+namespace QCVault.Tests
+{
+    [TestFixture]
+    public class LiveServerTests
+    {
+        public WebApplicationFactory<QCVault.Startup> factory;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            factory =
+                new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder => builder.UseSolutionRelativeContentRoot(""));
+        }
+
+        [OneTimeTearDown]
+        public void Teardown()
+        {
+            factory?.Dispose();
+        }
+    }
+}
+
