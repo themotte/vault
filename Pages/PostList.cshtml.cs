742dab91789163c0638135225298a881ff20bed6
Akamitori <akamitori@tutanota.com>
2022-01-23 17:45:01 +0200

    Added not found error for tags

--- Pages/PostList.cshtml.cs
+++ Pages/PostList.cshtml.cs
@@ -26,13 +26,17 @@ namespace QCVault.Pages
             this.postLoader = postLoader;
         }
 
-        public IActionResult OnGet(int? pageNumber, string? category)
+        public IActionResult OnGet(int? pageNumber, string category)
         {
             Posts = postLoader.Posts;
 
             if (!string.IsNullOrEmpty(category))
             {
                 Posts = Posts.Where(p => p.Category.Contains(category, StringComparer.OrdinalIgnoreCase)).ToList();
+                if (Posts.Count <= 0)
+                {
+                    return NotFound();
+                }
             }
 
             return Page();
