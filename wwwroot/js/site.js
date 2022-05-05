b71ea79f74f7271f1f355533e96387c29066d9f0
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2022-05-05 17:08:27 -0500

    Add spoiler tag support.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).on('click', '.spoiler, .spoiler-off', function (e) {
+  $(this).toggleClass('spoiler').toggleClass('spoiler-off');
+  e.stopPropagation();
+});
